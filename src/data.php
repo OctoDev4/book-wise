72bec62b58de36632674a1e5817a904e178a43bf
Felipe Almeida <felipe.alhopato@gmail.com>
2024-10-17 11:41:39 -0300

    add super globals

---
+++ src/data.php
@@ -0,0 +1,43 @@
+<?php 
+
+$books = [
+    [
+        'id' => 1,
+        'title' => 'O Senhor dos Anéis',
+        'author' => 'J.R.R. Tolkien',
+        'image' => 'o_senhor_dos_aneis.jpg',
+        'description' => 'Uma aventura épica na Terra Média.'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Harry Potter e a Pedra Filosofal',
+        'author' => 'J.K. Rowling',
+        'image' => 'harry_potter.jpg',
+        'description' => 'A história de um jovem bruxo e suas primeiras aventuras na Escola de Magia.'
+    ],
+    [
+        'id' => 3,
+        'title' => '1984',
+        'author' => 'George Orwell',
+        'image' => '1984.jpg',
+        'description' => 'Uma distopia sobre um futuro totalitário.'
+    ],
+    [
+        'id' => 4,
+        'title' => 'Orgulho e Preconceito',
+        'author' => 'Jane Austen',
+        'image' => 'orgulho_preconceito.jpg',
+        'description' => 'Um romance sobre amor e mal-entendidos na Inglaterra do século XIX.'
+    ],
+    [
+        'id' => 5,
+        'title' => 'O Hobbit',
+        'author' => 'J.R.R. Tolkien',
+        'image' => 'o_hobbit.jpg',
+        'description' => 'A jornada de Bilbo Bolseiro em uma aventura inesperada.'
+    ]
+];
+
+
+
+?>
