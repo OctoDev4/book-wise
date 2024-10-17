78113345507099335664de61a09196aec18ee642
Felipe Almeida <felipe.alhopato@gmail.com>
2024-10-16 23:24:32 -0300

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="input.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body class="bg-stone-950  text-stone-400">
+
+
+ <header>
+
+     <nav class="bg-red-500 flex mx- justify-between">
+         <div>BookWise</div>
+
+         <ul class="flex ">
+             <li><a>explorar</a></li>
+             <li><a>meus livros</a></li>
+             <li><a>explorar</a></li>
+
+         </ul>
+
+         <ul>
+             <li><a>Fazer Login</a></li>
+         </ul>
+
+
+     </nav>
+ </header>
+
+
+<main>
+    lista de livros
+</main>
+
+</body>
+</html>
