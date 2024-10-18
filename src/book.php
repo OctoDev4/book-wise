02e8fa9046ceb429f5fa908407c71bd2f305454a
Felipe Almeida <felipe.alhopato@gmail.com>
2024-10-18 14:38:02 -0300

    update dinamic pages

--- src/book.php
+++ src/book.php
@@ -8,11 +8,6 @@ $id = $_REQUEST['id'];
 $bookFilter =  array_filter($books , fn($data)=>$data['id'] == $id);
 $book = array_pop($bookFilter); 
 
-echo "<pre>";
-var_dump($book);
-echo"</pre>";
-
-
 
 ?>
 
@@ -55,8 +50,28 @@ echo"</pre>";
  </header>
 
 
-<main class="mx-auto max-w-screen-lg space-y-10">
-mudou aqui
+<main class="mx-auto max-w-screen-lg space-y-10 mt-10">
+
+<article class="p-6 rounded bg-stone-900 border-stone-800 border-2 ">
+      <div class="flex p-3">
+    <figure class="w-1/3">imagem</figure>
+    
+    
+    <header class="p-1">
+    
+     <h2 class="font-semibold"><a href="book.php?id=<?=$book['id']?>" class="hover:underline"><?=$book['title']?></a></h2>
+     <p class="text-xs italic"><?=$book['author']?></p>
+     <p class="text-xs italic"> ⭐⭐⭐⭐⭐ (3 avaliacoes)</p>
+    
+    </header>
+</div>
+ 
+
+ <div class="text-sm mt-4">
+ <?=$book['description']?>
+ </div>
+
+ </article>
 
 
 </main>
